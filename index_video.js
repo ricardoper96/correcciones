f32abc0f3d102c37354ffbbf9a9a0065e75eb92d
ricardo16 <ricardopernia22@gmail.com>
2022-08-22 16:38:51 -0400

    correcciones

--- index_video.js
+++ index_video.js
@@ -9,13 +9,11 @@
     const button_video2 = d.getElementById('button_video_2');
     const player_container= d.getElementById('team-video');
     const player_item =videojs(PLAYER_ID)
-    const source = d.getElementById('source')
 
 
     const handlerEventButton= async (button)=> {
         button.addEventListener('click',async () => {
             const link_video = button.value;
-            console.log(link_video)
 
 
             player_item.src(
